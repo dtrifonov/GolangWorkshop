b38343312ed2140745be88e8fa353a1fddc90528
Dimitar Trifonov <dimitar.trifonov@mentormate.com>
2018-10-06 08:52:41 +0000

    add diagnostics

--- cmd/go-sofia/main.go
+++ cmd/go-sofia/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
 	log.Print("Hello World")
-	err := http.ListenAndServe(":8080", nil)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	err := http.ListenAndServe(":8585", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.StatusTtext(http.StatusOK))
 }
