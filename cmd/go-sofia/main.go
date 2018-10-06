881f331a71e27610b17bb69630cad87ad5e05d03
Dimitar Trifonov <dimitar.trifonov@mentormate.com>
2018-10-06 09:54:01 +0000

    servers in goroutines

--- cmd/go-sofia/main.go
+++ cmd/go-sofia/main.go
@@ -1,35 +1,83 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
-
-	"github.com/dtrifonov/GolangWorkshop/internal/diagnostics"
+	"golang_workshop/internal/diagnostics"
 )
 
+type serverConf struct {
+	port   string
+	router http.Handler
+	name   string
+}
+
 func main() {
-	log.Print("Hello World")
+	log.Print("Starting the application...")
+
+	blPort := os.Getenv("PORT")
+	if len(blPort) == 0 {
+		log.Fatal("The application port should be set")
+	}
+
+	diagPort := os.Getenv("DIAG_PORT")
+	if len(diagPort) == 0 {
+		log.Fatal("The diagnostics port should be set")
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
 
-	go func() {
-		err := http.ListenAndServe(":8080", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
+	diagnostics := diagnostics.NewDiagnostics()
 
-	err := http.ListenAndServe(":8585", nil)
-	if err != nil {
-		log.Fatal(err)
+	possibleErrors := make(chan error, 2)
+
+	configurations := []serverConf{
+		{
+			port:   blPort,
+			router: router,
+			name:   "application server",
+		},
+		{
+
+			port:   diagPort,
+			router: diagnostics,
+			name:   "diagnostics server",
+		},
 	}
 
+	servers := make([]*http.Server, 2)
+
+	for i, c := range configurations {
+		go func(conf serverConf, i int) {
+			log.Printf("The %s is preparing to handle connections...", conf.name)
+			servers[i] = &http.Server{
+				Addr:    ":" + conf.port,
+				Handler: conf.router,
+			}
+			err := servers[i].ListenAndServe()
+			if err != nil {
+				possibleErrors <- err
+			}
+		}(c, i)
+	}
+
+	select {
+	case err := <-possibleErrors:
+		for _, s := range servers {
+			// propose a PR with context timeout
+			s.Shutdown(context.Background())
+		}
+		log.Fatal(err)
+	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	log.Print("The hello handler was called")
 	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
